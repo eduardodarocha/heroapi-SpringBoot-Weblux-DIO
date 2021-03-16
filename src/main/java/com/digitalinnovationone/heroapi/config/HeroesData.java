1abdd566b7f3e62ed105f32430c6fecc9fd7eca6
Eduardo Rocha <eduardomfrocha@gmail.com>
2021-03-16 10:17:54 -0300

    Primeiro commit da API Gerenciando heróis

---
+++ src/main/java/com/digitalinnovationone/heroapi/config/HeroesData.java
@@ -0,0 +1,48 @@
+package com.digitalinnovationone.heroapi.config;
+
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.PutItemOutcome;
+import com.amazonaws.services.dynamodbv2.document.Table;
+import static com.digitalinnovationone.heroapi.constants.HeroesConstant.ENDPOINT_DYNAMO;
+import static com.digitalinnovationone.heroapi.constants.HeroesConstant.REGION_DYNAMO;
+
+public class HeroesData {
+    public static void main(String[] args) throws Exception {
+
+        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(ENDPOINT_DYNAMO, REGION_DYNAMO))
+                .build();
+        DynamoDB dynamoDB = new DynamoDB(client);
+
+        Table table = dynamoDB.getTable("Heroes_Api_Table");
+
+        Item hero = new Item()
+                .withPrimaryKey("id", "2")
+                .withString("name", "Mulher Maravilha")
+                .withString("universe", "DC Comics")
+                .withNumber("films", 2);
+
+        Item hero2 = new Item()
+                .withPrimaryKey("id", "3")
+                .withString("name", "Viuva Negra")
+                .withString("universe", "Marvel")
+                .withNumber("films", 2);
+
+        Item hero3 = new Item()
+                .withPrimaryKey("id", "4")
+                .withString("name", "Capitã Marvel")
+                .withString("universe", "Marvel")
+                .withNumber("films", 2);
+
+        PutItemOutcome outcome1 = table.putItem(hero);
+        PutItemOutcome outcome2 = table.putItem(hero2);
+        PutItemOutcome outcome3 = table.putItem(hero3);
+
+    }
+
+}
+
